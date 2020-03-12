528e7ed8d683236f231ed65a2345302f60fbe4eb
Andrea Di Cesare <andrea@softinstigate.com>
2020-03-12 19:39:52 +0100

    Start palette component

--- src/app/palette/palette.component.ts
+++ src/app/palette/palette.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Palette } from 'src/app/model';
 
 @Component({
   selector: 'app-palette',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./palette.component.css']
 })
 export class PaletteComponent implements OnInit {
+  @Input() palette: Palette;
 
   constructor() { }
 
