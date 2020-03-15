b972f1e19497499706940716ba741dc68faeb532
Andrea Di Cesare <andrea@softinstigate.com>
2020-03-15 11:42:35 +0100

    Add draft about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/app/home/home.component';
 import { PalettesComponent } from 'src/app/palettes/palettes.component';
 import { NewPaletteComponent } from 'src/app/new-palette/new-palette.component';
+import { AboutComponent } from 'src/app/about/about.component';
 
 const routes: Routes = [
-  { path: '',      component: HomeComponent },
+  { path: '',         component: HomeComponent },
   { path: 'palettes', component: PalettesComponent },
   { path: 'new',      component: NewPaletteComponent },
+  { path: 'about',    component: AboutComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
